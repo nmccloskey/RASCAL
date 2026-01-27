c43db5cc779c3d31eb51b77c16bd6bd232ff4ce8
nmccloskey <mccloskey.nick@gmail.com>
2026-01-27 09:32:42 -0500

    clean up dependencies & version

--- src/rascal/__init__.py
+++ src/rascal/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "1.0.6"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("rascal-speech")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
