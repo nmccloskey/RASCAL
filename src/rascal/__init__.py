149829ab72286bfafc06d90ac79b97daea4e31eb
nmccloskey <mccloskey.nick@gmail.com>
2026-06-22 14:51:03 -0400

    archive most code & docs to prepare for wrapper release; update gitignore

--- src/rascal/__init__.py
+++
@@ -1,6 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("rascal-speech")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.0.0"
