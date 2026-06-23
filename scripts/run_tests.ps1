4cdd826144175ee1385bf1030c3ae5c316c66199
nmccloskey <mccloskey.nick@gmail.com>
2026-06-23 19:30:28 -0400

    tests: add helper script for Windows

---
+++ scripts/run_tests.ps1
@@ -0,0 +1,18 @@
+param(
+    [string[]]$PytestArgs = @("tests")
+)
+
+$ErrorActionPreference = "Stop"
+
+$Python = if ($env:RASCAL_PYTHON) {
+    $env:RASCAL_PYTHON
+} else {
+    "$env:USERPROFILE\anaconda3\envs\diaad\python.exe"
+}
+
+if (-not (Test-Path $Python)) {
+    throw "Could not find RASCAL Python at $Python. Set RASCAL_PYTHON to override."
+}
+
+& $Python -m pytest @PytestArgs
+exit $LASTEXITCODE
